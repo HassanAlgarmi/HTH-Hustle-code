505f26aa192f660a1aa4e64119ca2e03cea2693d
HassanAlgarmi <hassanalgarmi@gmail.com>
2025-06-18 10:35:53 -0700

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,15 @@
+name = "hassan algarmi"
+
+nickname = "hassan"
+
+age = 16
+
+used_python = 'true' 
+
+hobbies = ["playing videogames", "fishing",]
+
+fav_things = {"movie": "minecraft movie", "food": "mayonaise"}
+
+##extra variables
+
+sig = "ma"
